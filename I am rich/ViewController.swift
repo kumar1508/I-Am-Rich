2079b3aac1d70af3531ca6ba4cbf7da159485e3e
Kumar Sashwat <kumarsashwat@Kumars-MacBook-Air.local>
2023-07-16 08:09:33 +0530

    first commit

---
+++ I am rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am rich
+//
+//  Created by Kumar Sashwat on 13/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    }
+
